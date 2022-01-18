a32f574f2b608718360e1a60d4096fe6e5cb14ba
chandu <ramanasatya626@gmail.com>
2022-01-18 08:34:07 +0530

    included db

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -22,21 +22,21 @@ const getters = {
 const actions = {
   async fetchTodos({ commit }) {
     const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/todos'
+      'http://localhost:8081/todos'
     );
 
     commit('setTodos', response.data);
   },
   async addTodo({ commit }, title) {
     const response = await axios.post(
-      'https://jsonplaceholder.typicode.com/todos',
+      'http://localhost:8081/todos',
       { title, completed: false }
     );
 
     commit('newTodo', response.data);
   },
   async deleteTodo({ commit }, id) {
-    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    await axios.delete(`http://localhost:8081/todos/${id}`);
 
     commit('removeTodo', id);
   },
@@ -47,14 +47,14 @@ const actions = {
     );
 
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+      `http://localhost:8081/todos?_limit=${limit}`
     );
 
     commit('setTodos', response.data);
   },
   async updateTodo({ commit }, updTodo) {
     const response = await axios.put(
-      `https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,
+      `http://localhost:8081/todos/${updTodo.id}`,
       updTodo
     );
 
